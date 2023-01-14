c9fbf69bfb02b660d114e5e37840fa703eb8fa89
Code Your Moods <72027864+PrathamAyush@users.noreply.github.com>
2023-01-14 13:52:33 +0530

    Add files via upload

---
+++ to-do-app/src/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+import './App.css';
+import ToDoApp from './to-do';
+
+function App() {
+  return (
+    <div style={{backgroundColor:"oldlace",minHeight:"100vh"}}>
+    <div>  
+        <ToDoApp />
+     
+    </div>
+    </div>
+  );
+}
+
+export default App;
